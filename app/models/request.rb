b570254c1cec9cc1a5d1a00f811d9324f9df81ff
marco varela <marco.varela12@gmail.com>
2020-03-20 00:27:50 +0000

    some work on views

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,7 +4,7 @@ class Request < ApplicationRecord
 
   validates :title, presence: true
   validates :description, presence: true, length: { maximum: 500 }
-  validates :category, inclusion: { in: ["compras", "obras", "recados", "saúde", "cuidados", "donativos"] }
+  validates :category, inclusion: { in: ["compras", "obras/reparações", "recados", "saúde", "cuidados", "donativos", "outra"] }
   validates :person_name, presence: true
   validates :age, numericality: true
   validates :address, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -55,12 +55,12 @@ ActiveRecord::Schema.define(version: 2020_03_19_130018) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.datetime "birth_date", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.date "birth_date"
     t.text "address"
     t.integer "zip_code"
-    t.string "city", null: false
+    t.string "city"
     t.text "about_me"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
