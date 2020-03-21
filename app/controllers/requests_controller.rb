99f6714ca7972a85748b3313e76b5d1c4934cc3e
marco varela <marco.varela12@gmail.com>
2020-03-21 19:39:52 +0000

    Added map to request show and created request new

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,18 +3,13 @@ class RequestsController < ApplicationController
 
   def index
     @requests_near = Request.where(city: current_user.city)
-    @requests = Request.where.not(city: current_user.city)
+    @requests_far = Request.where.not(city: current_user.city).geocoded
     @requests = Request.geocoded
-    @markers = @requests.map do |request|
-      {
-        lat: request.latitude,
-        lng: request.longitude,
-        infoWindow: render_to_string(partial: "shared/info_window", locals: { request: request })
-      }
-    end
+    map_markers(@requests)
   end
 
   def show
+    single_map_marker
   end
 
   def new
@@ -23,6 +18,7 @@ class RequestsController < ApplicationController
 
   def create
     @request = Request.new(request_params)
+    @request.user = current_user
     if @request.save
       redirect_to request_path(@request)
     else
@@ -53,5 +49,24 @@ class RequestsController < ApplicationController
 
   def request_params
     @request = params.require(:request).permit(:title, :description, :category, :person_name, :age, :address, :zip_code, :city, :phone_number)
+  end
+
+  def map_markers(marker)
+    @markers = marker.map do |marker|
+      {
+        lat: marker.latitude,
+        lng: marker.longitude,
+        infoWindow: render_to_string(partial: "shared/info_window", locals: { request: marker })
+      }
+    end
+  end
+
+  def single_map_marker
+    @markers = [
+      {
+        lat: @request.latitude,
+        lng: @request.longitude,
+        infoWindow: render_to_string(partial: "shared/info_window", locals: { request: @request })
+      }]
   end
 end
