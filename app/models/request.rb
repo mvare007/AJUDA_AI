e104475e9665b388247820d31044668e2878095f
marco varela <marco.varela12@gmail.com>
2020-03-22 16:00:48 +0000

    Finished new request form added carousel to home page

--- app/models/request.rb
+++ app/models/request.rb
@@ -6,10 +6,9 @@ class Request < ApplicationRecord
 
   validates :title, presence: true
   validates :description, presence: true, length: { maximum: 500 }
-  validates :category, inclusion: { in: ["Compras", "Reparações", "Recados", "Saúde", "Cuidados", "Donativos", "Companhia", "Associação", "Outro"] }
-  validates :age, numericality: true
+  validates :category, inclusion: { in: ["Compras", "Reparações", "Recados", "Saúde", "Cuidados", "Donativos", "Companhia", "Associação", "Animais", "Denúncia", "Outro"] }
   validates :address, presence: true
-  validates :zip_code, presence: true, format: { with: /\d{4}\-\d{3}/, message: "Por favor introduz um Código Postal válido"}
+  validates :zip_code, presence: true, format: { with: /\d{4}\-\d{3}/}
   validates :city, presence: true
 
 end
