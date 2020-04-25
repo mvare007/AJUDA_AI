5a38c16c2226f1288421b20b18f213e8f60bf9ac
marco varela <marco.varela12@gmail.com>
2020-04-25 18:25:43 +0100

    Add more than 1 volunteer per request

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,7 +1,7 @@
 class Assignment < ApplicationRecord
   belongs_to :request
   belongs_to :asker, class_name: "User"
-  belongs_to :volunteer
+  belongs_to :volunteer, optional: true
 
   validates :asker_id, presence: true
   validates :request_id, presence: true
