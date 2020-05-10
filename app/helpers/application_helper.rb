b1847a0e2a3329d7d6903aabfa58f3edb373beb2
marco varela <marco.varela12@gmail.com>
2020-05-10 22:40:55 +0100

    Added review and mean user rating to user show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 module ApplicationHelper
-
+  def mean_review(user_reviews)
+    ratings_array = []
+    user_reviews.each do |review|
+      ratings_array << review.rating
+    end
+    mean = ratings_array.sum(0.0) / ratings_array.size
+  end
 end
+
