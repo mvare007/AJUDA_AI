ffaffc5e9655f7b3db64ba3ebcf109f69283c4dd
marco varela <marco.varela12@gmail.com>
2020-07-04 22:55:56 +0100

    Fixed typo in config production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://ajuda-ai-beta.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://ajuda-ai-beta.herokuapp.com/cable', /http:\/\/ajuda-ai-beta.herokuapp.com/cable.*/ ]
+  config.action_cable.allowed_request_origins = [ 'http://ajuda-ai-beta.herokuapp.com/cable', /http:\/\/ajuda-ai-beta.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
